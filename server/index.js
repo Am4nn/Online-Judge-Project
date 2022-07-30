aeaa2bc6a65d09cb6ab312f16adb0d75b1e50767
Aman Arya <125aryaaman@gmail.com>
2022-07-31 01:53:17 +0530

    corrected error handling

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const explore = require('./routes/explore');
 const user = require('./routes/user');
 const app = express();
 const { connectDB } = require('./DataBase/connectDB');
+const path = require('path');
 
 connectDB();
 
@@ -26,6 +27,18 @@ app.use('/api/user', user);
 // set 404 route
 
 // set handle error
+
+
+// Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+// Set static folder
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+);
+// }
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
